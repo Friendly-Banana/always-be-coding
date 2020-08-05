9fcc90fb16004f9d666804e50154d8322b78a0f0
Just-Learned-It <69007475+Just-Learned-It@users.noreply.github.com>
2020-08-05 16:14:32 +0200

    Create Queue.py

---
+++ Queue.py
@@ -0,0 +1,15 @@
+"""
+basic Queue
+uses a List under the hood
+"""
+class Queue():
+    def __init__(self):
+        self.items = []
+    
+    def enqueue(item):
+        items.append(item)
+    
+    def dequeue():
+        # list is not empty
+        if items:
+            return items.pop(len(self.items) - 1)
